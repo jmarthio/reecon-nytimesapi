3412a58df2c5243360a33457368ba533bb03d626
jmarthio <joshuamarthio7@gmail.com>
2022-09-07 12:44:53 +0700

    update MUI News Article Component

--- components/NewsArticle.js
+++ components/NewsArticle.js
@@ -1,16 +1,17 @@
+import { Grid } from "@mui/material";
 import React from "react";
 import NewsArticleStyles from "../styles/newsArticle.module.css";
 import NewsItem from "./NewsItem";
 
 const NewsArticle = ({ newsArticle }) => {
   return (
-    <div className={NewsArticleStyles.grid}>
+    <Grid className={NewsArticleStyles.grid}>
       {newsArticle.results.map((article) => (
         <React.Fragment key={article.id}>
           <NewsItem article={article} />
         </React.Fragment>
       ))}
-    </div>
+    </Grid>
   );
 };
 
